61130c0d92ad0fd0b201040e309775e48be3ed69
oskrgab <ogcg12@gmail.com>
2024-01-15 00:29:23 +0000

    Finished "for" examples

--- posts/for_loop_options.py
+++ posts/for_loop_options.py
@@ -1,7 +1,7 @@
 # Original post
 def make_engineering(subjects):
   for i in range(len(subjects)):
-    print(subjects[i] + ' Eng. ')
+    print(subjects[i] + ' Eng.')
 
 # List of subjects
 my_subjects = [
@@ -11,33 +11,38 @@ my_subjects = [
   'Electrical'
 ]
 
+print("Using non-pythonic for loop:")
 make_engineering(my_subjects)
 
 # Explanation of other for loops
 
-# 1. Iterate over each item
+# 1. Iterate using the "in" keyword
 def make_engineering_1(subjects):
   for subject in subjects:
-    print(subject + ' Eng. ')
+    print(subject + ' Eng.')
 
+print("\nUsing for loop with 'in':")
 make_engineering_1(my_subjects)
 
-# 2. Using list comprehension
+# 2. Using list comprehensions
 def make_engineering_2(subjects):
-  return [subject + ' Eng. ' for subject in subjects]
+  return [print(subject + ' Eng.') for subject in subjects]
 
+print("\nUsing list comprehension:")
 make_engineering_2(my_subjects)
 
 # 3. Using map
 def make_engineering_3(subjects):
-  return list(map(lambda subject: subject + ' Eng. ', subjects))
+  return list(map(lambda x: print(x + ' Eng.'), subjects))
 
+print("\nUsing map:")
 make_engineering_3(my_subjects)
 
 # 4. Using enumerate
 def make_engineering_4(subjects):
   for i, subject in enumerate(subjects):
-    print(f'{i}: {subject} Eng. ')
+    print(f'{i}: {subject} Eng.')
 
+print("\nUsing enumerate:")
 make_engineering_4(my_subjects)
 
